2f4392a960b4fc4bd70696d80009806474d3a90c
Eric Laukien <ericlaukien@gmail.com>
2023-02-20 11:45:30 -0500

    BossFight development

---
+++ games/bossfight/common_components.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "ecs.h"
+#include "common_assets.h"
+
+// General
+struct Component_Transform {
+    Vector2 position{ 0.0f, 0.0f };
+    float rotation = 0.0f;
+    float scale = 1.0f;
+};
+
+// Physics
+struct Component_Collision {
+    Rectangle bounds{ -0.5f, -0.5f, 1.0f, 1.0f };
+};
+
+struct Component_Dynamics {
+    Vector2 velocity{ 0.0f, 0.0f };
+};
+
+// Sprites
+struct Component_Sprite {
+    // Relative to transform
+    Vector2 position{ 0.0f, 0.0f };
+    float rotation = 0.0f;
+    float scale = 1.0f;
+    bool flip_x = false;
+
+    Color tint{ 255, 255, 255, 255 };
+    float z = 0.0f; // Ordering
+
+    Asset_Texture* texture = nullptr;
+};
+
+struct Component_Mob_AI {
+};
+
+// Reset-triggering
+struct Component_Hazard {
+    bool destroyable = false;
+};
+
+struct Component_Goal {};
+
+struct Component_Agent {
+    int action = 0;
+};
+
+struct Particle {
+    Vector2 position;
+    Vector2 dir;
+    float rotation;
+    float life = 0.0f;
+};
+
+struct Component_Particles {
+    std::vector<Particle> particles;
+
+    Vector2 offset{ 0.0f, 0.0f };
+    float lifespan = 3.0f;
+    float spawn_timer = 0.0f;
+    float spawn_time = 0.3f;
+    bool enabled = true;
+};
---
+++ games/bossfight/common_systems.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include "common_components.h"
+#include "common_assets.h"
+#include "ecs.h"
+
+#include <cmath>
+#include <algorithm>
+
+// -------------------- Sprites ---------------------
+//
+// Selector for sprites to render
+enum Sprite_Render_Mode {
+    all,
+    positive_z,
+    negative_z
+};
+
+// Sprite rendering system
+class System_Sprite_Render : public System {
+private:
+    std::vector<std::pair<float, Entity>> render_entities;
+
+public:
+    void update(float dt);
+    void render(Sprite_Render_Mode mode);
+
+    void clear_render() {
+        render_entities.clear();
+    }
+};
+
+// -------------------- Hazards --------------------
+
+// Empty mostly, since just need it to collect hazards for agent system
+class System_Hazard : public System {
+public:
+    std::unordered_set<Entity> &get_entities() {
+        return entities;
+    }
+};
+
+class System_Mob_AI : public System {
+public:
+    void update(float dt);
+};
+
+// -------------------- Goals --------------------
+
+// Empty mostly, since just need it to collect goals for agent system
+class System_Goal : public System {
+public:
+    std::unordered_set<Entity> &get_entities() {
+        return entities;
+    }
+};
+
+// --------------------- Player --------------------
+
+class System_Agent : public System {
+public:
+    struct Bullet {
+        Vector2 pos;
+        Vector2 vel;
+        float rotation;
+        float frame = -1.0f; // Animation frame, -1.0f is "dead" flag
+    };
+
+private:
+    // Agent textures
+    Asset_Texture ship_texture;
+    Asset_Texture bullet_texture;
+    std::vector<Bullet> bullets;
+    std::vector<Asset_Texture> explosion_textures;
+    int next_bullet = 0;
+    int num_bullets = 0;
+    float bullet_timer = 0.0f;
+
+public:
+    void init(); // Needs to load sprites
+
+    // Returns alive status (false if touched hazard), and whether touched a goal (carrot), and number of hazards destroyed
+    std::tuple<bool, bool, int> update(float dt, const std::shared_ptr<System_Hazard> &hazard, const std::shared_ptr<System_Goal> &goal, int action);
+    void render();
+
+    void reset() {
+        next_bullet = 0;
+        num_bullets = 0;
+        bullet_timer = 0.0f;
+    }
+};
+
+// ------------------- Particles ------------------
+
+class System_Particles : public System {
+private:
+    Asset_Texture particle_texture;
+
+public:
+    void init(); // Loads sprites
+    
+    void update(float dt);
+    void render();
+};
